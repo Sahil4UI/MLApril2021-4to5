a01ac28583025b4ff7a3703ee4a431df53598866
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2021-04-20 15:59:51 +0530

    Create Loops01.py

---
+++ Loops01.py
@@ -0,0 +1,80 @@
+#find the sum of first 10 numbers(1 to 10)
+'''
+result=0
+for i in range(1,11):
+    result += i
+print(result)
+'''
+#fibonacci Series : 0 1 1 2 3 5 8 13......
+'''
+f = 0
+s = 1
+print(f)
+print(s)
+
+for i in range(8):
+    t = f+s
+    print(t)
+    f=s
+    s=t
+'''
+#Break statement & Continue Statement
+'''
+for i in range(10000000000000):
+
+    print(i)
+
+    if i==10:
+        break
+'''
+#continue
+for i in range(1,21):
+    if i%2==0:
+        continue
+    print(i)
+
+'''
+for i in range(1,11):
+    print(i)
+'''
+#while loop
+'''
+f = 0
+s = 1
+print(f)
+print(s)
+
+for i in range(8):
+    t = f+s
+    print(t)
+    f=s
+    s=t
+'''
+
+f = 0
+s = 1
+print(f)
+print(s)
+x=1
+while x<=8:
+    t = f+s
+    print(t)
+    f=s
+    s=t
+    x+=1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
